a0ebe73ad3738b4c86fd74a28213f9c940e0ed89
Ilpo Juvander <ilpo_juvander@hotmail.com>
2016-12-27 11:01:19 +0200

    Fixed version numbering

--- DotNetNuke.Repository/AssemblyInfo.vb
+++ DotNetNuke.Repository/AssemblyInfo.vb
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("03.05.07.*")>
+<Assembly: AssemblyVersion("03.05.08.*")>
